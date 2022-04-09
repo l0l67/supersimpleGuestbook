cea77c112e561ec1ebcfef7555b882d0c58d5a1f
l0l67 <jonathann34555@gmail.com>
2022-04-10 00:14:15 +0200

    post timeout added

--- DB.py
+++ DB.py
@@ -11,7 +11,25 @@ def getMessages():
     global db
     cursor = db.execute('select * from guestbook')
     return cursor.fetchall()
-    
+
+def setLastPost(ip):
+    global db
+    if _IPexists(ip):
+        db.execute('update lastplaced set postdate = CURRENT_TIMESTAMP where ip_addr = ?', [ip])
+    else:
+        db.execute('insert into lastplaced (ip_addr, postdate) values (?, CURRENT_TIMESTAMP)', [ip])
+
+def _IPexists(ip):
+    global db
+    cursor = db.execute('select exists(select 1 from lastplaced where ip_addr = ?)', [ip]).fetchone()[0]
+    print(cursor)
+    return cursor == 1
+
+def getLastPost(ip):
+    global db
+    cursor = db.execute("select ip_addr, datetime(postdate, 'localtime') from lastplaced where ip_addr = ?", [ip])
+    return cursor.fetchall()
+
 def closeDB():
     global db
     db.close()
@@ -20,6 +38,7 @@ def closeDB():
 def createTable():
     global db
     db.execute('create table if not exists guestbook (id integer primary key AUTOINCREMENT, username text not null, message text, website text, entrydate datetime not null);')
+    db.execute('create table if not exists lastplaced (id integer primary key AUTOINCREMENT, ip_addr text not null, postdate datetime not null)')
 
 if __name__ == '__main__':
     createTable()
--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 from flask import *
+from werkzeug.middleware.proxy_fix import ProxyFix
+from datetime import datetime
 import DB
 
+# Timeout duration in seconds for IP:
+post_timeout = 60
+
 public = Flask(__name__)
+public.wsgi_app = ProxyFix(public.wsgi_app, x_for=1, x_host=1)
 
 @public.route('/', methods=['GET'])
 def index():
     entries = DB.getMessages()
     entries.reverse()
-    
+
     return render_template('index.html', entries=entries)
 
 @public.route('/guestbook', methods=['POST'])
 def guestbook():
     tmp = request.form
-    if 'username' in tmp and tmp.get('username') != '':
+
+    if ('username' in tmp and tmp.get('username') != '') and canPlace(request):
         DB.newMessage(tmp.get('username'), tmp.get('message')[:325], tmp.get('website'))
     return redirect(url_for('index'))
 
+def canPlace(request):
+    global post_timeout
+
+    ip = request.remote_addr
+    last_post = DB.getLastPost(ip)
+
+    if len(last_post) > 0:
+        last_post = datetime.strptime(last_post[0][1], '%Y-%m-%d %H:%M:%S')
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        now = datetime.strptime(now, '%Y-%m-%d %H:%M:%S')
+        
+        diff = now - last_post
+
+        if diff.total_seconds() <= post_timeout:
+            return False
+    
+    DB.setLastPost(ip)
+    return True
+
 if __name__ == '__main__':
-    public.run(port=80)
+    public.run(port=42069, host='0.0.0.0')
